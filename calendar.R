33b84bd9253e9225b885250c30c7eb494a19f88e
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2024-05-03 14:18:47 +1000

    pacakge design 2

--- calendar.R
+++ calendar.R
@@ -4,6 +4,7 @@ library(tibble)
 library(lubridate)
 library(yaml)
 library(ggplot2)
+library(ggiraph)
 
 ## for YAML collections
 yaml_date_nodes_to_tbl <- function(list) {
@@ -36,24 +37,3 @@ tidy_date_df_export <- function(df) {
 write_yaml_col_maj_FALSE <- function(x, file) {
     write_yaml(x, file, column.major = FALSE)
 }
-
-
-
-## Create a calendar
-
-schedules_data <- here::here("schedules.yml") |> read_yaml()
-
-key_dates_schedules <- schedules_data |>
-    yaml_date_nodes_to_tbl() |>
-    tidy_date_tbl() |>
-    verify_date_df() |>
-    dplyr::mutate(id = row_number()) |>
-    dplyr::relocate(id, .before = details)
-
-numbat_long <- key_dates_schedules |>
-    reframe_events(startDate, endDate)
-
-
-numbat_long |>
-    gg_facet_wrap_months(unit_date) +
-    geom_text(aes(label = categories), nudge_y = -0.25, na.rm = TRUE)
