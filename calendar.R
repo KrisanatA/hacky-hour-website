9a1b57b832a0f7303b41bfdc8e91d4d049c23778
janithwanni <janithcwanni@gmail.com>
2024-07-21 19:40:19 +1000

    Change schedule structure and update with new dates

--- calendar.R
+++ calendar.R
@@ -37,3 +37,34 @@ tidy_date_df_export <- function(df) {
 write_yaml_col_maj_FALSE <- function(x, file) {
     write_yaml(x, file, column.major = FALSE)
 }
+
+plot_calendar <- function(tbl) {
+    tbl |>
+        gg_facet_wrap_months(unit_date) +
+        geom_text(aes(label = emoji), nudge_y = -0.25, nudge_x = -0.1, size = 3.5, na.rm = TRUE) +
+        geom_tile_interactive(
+            aes(
+                tooltip = paste(
+                    details |> tidyr::replace_na(""),
+                    ifelse(is.na(details), "", "@"),
+                    room |> tidyr::replace_na("")),
+                data_id = id
+            ),
+            alpha = 0.2,
+            fill = "transparent",
+            colour = "grey80"
+        )
+}
+
+render_calendar <- function(yaml_file) {
+    read_yaml(yaml_file) |>
+        yaml_date_nodes_to_tbl() |>
+        tidy_date_tbl() |>
+        verify_date_df() |>
+        dplyr::mutate(id = row_number()) |>
+        dplyr::relocate(id, .before = details) |>
+        reframe_events(startDate, endDate) |>
+        plot_calendar()
+}
+
+
